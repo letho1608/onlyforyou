e12ec4418901eda02cfcf336d7b2ad004fcc2cb2
Le Quang Tho <92069270+letho1608@users.noreply.github.com>
2023-03-08 02:03:10 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'Tam Tam,';
+const eventName = 'Chúc Mừng 8/3';
+const titleCard = 'Gửi tới em';
+const giftUrl = 'https://www.facebook.com/profile.php?id=100038603851959';
+const contentCard = 'Chúc bé 8/3 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với bé trong hôm nay và cả những ngày sau';
+
+
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
